1746b5b384246c409710a1af1a4aaa554207b4e1
Stas <stazzzdzr@gmail.com>
2021-11-19 00:18:50 +0300

    19.11

--- try-your-knowlege/js/script.js
+++ try-your-knowlege/js/script.js
@@ -1,26 +1,21 @@
 
-Object.prototype.logInfo = function(profession){
-	console.group(`Log info ${this.name}`)
-	console.log(`name: ${this.name}`)
-	console.log(`age: ${this.age}`)
-	console.log(`profession: ${profession}`)
-	console.groupEnd()
+
+
+
+
+
+
+function logPerson(){
+	console.log(`Person: ${this.name} age: ${this.age}`)
 }
 
-const nika ={
-	name: 'Nika',
-	age: 28
+
+function bind(context, fn){
+	return function(...args){
+		fn.apply(context, args)
+	}
 }
 
-const person = {
-	name: 'Stas',
-	age: 32,
-}
+const person1 = {name: 'stas', age: 22}
 
-nika.logInfo = person.logInfo
-//nika.logInfo('manager')
-//person.logInfo('kladovchik')
-
-const arr = [1, 5, 7]
-
-Array.prototype.mult = function(n){ return this.map(e => e*n)}
+bind(person1, logPerson)()
